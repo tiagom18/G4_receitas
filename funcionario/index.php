f9dcea44f789e98fb6455bbd8153fdc6dc1243b6
tiagom18 <72321841+tiagom18@users.noreply.github.com>
2021-11-09 21:20:30 -0300

    acabou 1 aee

--- funcionario/index.php
+++ funcionario/index.php
@@ -21,6 +21,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $nome_fantasia = filter_input(INPUT_POST,'nome_fantasia');
     $Usuario = filter_input(INPUT_POST,'Usuario');
     $senha = filter_input(INPUT_POST,'senha');
+    $id_Cargo = filter_input(INPUT_POST,'id_Cargo');
 } else if (!isset($id_Funcionario)){
     $id_Funcionario = (isset($_GET["id_Funcionario"]) && $_GET["id_Funcionario"] != null) ? $_GET["id_Funcionario"] : "";
 }
@@ -33,15 +34,17 @@ if (isset($_REQUEST['act']) && $_REQUEST['act'] == "save" && $nome != "") {
             $stmt->bindParam(":id_Funcionario", $id_Funcionario);
           
         } else {
-            $stmt = $conexao->prepare("INSERT INTO g4_funcionario(nome, rg, data_ingresso, nome_fantasia, Usuario, senha) 
-            VALUES (:nome,:rg,:data_ingresso,:nome_fantasia,:Usuario,:senha)");
+            $stmt = $conexao->prepare("INSERT INTO g4_funcionario(nome, rg, data_ingresso, nome_fantasia, Usuario, senha, id_Cargo) 
+            VALUES (:nome,:rg,:data_ingresso,:nome_fantasia,:Usuario,:senha,:id_Cargo)");
         }
+     
         $stmt->bindParam(":nome", $nome ,PDO::PARAM_STR);
         $stmt->bindParam(":rg", $rg,PDO::PARAM_STR);
         $stmt->bindParam(":data_ingresso", $data_ingresso ,PDO::PARAM_STR);
         $stmt->bindParam(":nome_fantasia", $nome_fantasia ,PDO::PARAM_STR);
         $stmt->bindParam(":Usuario", $Usuario ,PDO::PARAM_STR);
         $stmt->bindParam(":senha", $senha ,PDO::PARAM_STR);
+        $stmt->bindParam(":id_Cargo", $id_Cargo ,PDO::PARAM_STR);
         
         
 
@@ -55,6 +58,7 @@ if (isset($_REQUEST['act']) && $_REQUEST['act'] == "save" && $nome != "") {
                 $nome_fantasia = null;
                 $Usuario = null;
                 $senha = null;
+                $id_Cargo = null;
             } else {
                 echo "<p>Erro no cadastro do cargo</p>";
             }
@@ -71,11 +75,11 @@ if (isset($_REQUEST['act']) && $_REQUEST['act'] == "save" && $nome != "") {
 if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id_Funcionario != ""){
     try {
         echo "id_Funcionario :",  $id;
-<<<<<<< HEAD
+
         $stmt = $conexao->prepare("SELECT a.id_Funcionario, a.nome, a.rg, a.data_ingresso, a.nome_fantasia, a.Usuario, a.senha, b.id_Cargo FROM g4_funcionario as a INNER JOIN g4_cargo as b on a.id_Cargo = b.id_Cargo WHERE id_Funcionario= :id");
-=======
+
         $stmt = $conexao->prepare("SELECT id_Funcionario, nome, rg, data_ingresso, nome_fantasia, Usuario, senha FROM g4_funcionario WHERE id_funcionario= :id");
->>>>>>> 4661c714e938f21e8819d6edd27dc4f4e2c4ad92
+
         $stmt->bindParam(":id", $id_Funcionario, PDO::PARAM_INT);
         if ($stmt->execute()) {
             $rs = $stmt->fetch(PDO::FETCH_OBJ);
@@ -94,8 +98,23 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id_Funcionario != "
     }
 }
 
+//pegar as opções do banco 
+
+    $sql = " SELECT * FROM g4_cargo";
+    try {
+        $stmt = $conexao -> prepare($sql);
+        $stmt -> execute();
+        $results = $stmt -> fetchAll();
+    }
+    catch(Exception $ex){
+        echo ($ex -> getMessage());
+
+    }
+    
+
 
 ?>
+
 <body>
     <!--Inicio - Insert form-->
 
@@ -148,7 +167,15 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id_Funcionario != "
         echo (isset($senha) && ($senha != null || $senha != "")) ? $senha : '';
         ?>" />
         </br>
-        <!-- tem q fazer um form de cargo -->
+        
+<select id="id_Cargo" name="id_Cargo">
+        <option>Cargo</option>
+        <?php foreach($results as $output) {?>
+    <option value="<?php echo $output["id_Cargo"];?>"><?php echo $output["nome"];?></option>
+        <?php } ?>
+    </select>
+
+    
         <button type="submit" class = "">Salvar</button>
         <button type="reset" class = "">Cancelar</button>
         <hr>
