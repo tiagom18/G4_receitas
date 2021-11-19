d02c29ab06a55665af8ca30e9a22f788d8240071
Gabriel Stênio <gabri.stnio50@hotmail.com>
2021-11-19 00:50:22 -0300

    css exluir.php cargo

--- cargo/excluir.php
+++ cargo/excluir.php
@@ -17,68 +17,80 @@
         //Aprensentar dados do cargo selecionado para exclui para o usuário confirmar se realmente quer cancelar-->
         $id_Cargo=$_GET["id"];
     ?>
-    <h3>Você está tentando excluir:</h3>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Descrição</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                try {
-                    $stmt = $conexao->prepare("SELECT * FROM g4_cargo WHERE id_Cargo= :id");
-                    $stmt->bindParam(":id", $id_Cargo, PDO::PARAM_INT); 
-                    if ($stmt->execute()) {
-                        while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
-                            echo "<tr>";
-                            echo "<td>$rs->id_Cargo</td>";
-                            echo "<td>$rs->nome</td>";
-                            echo "</tr>";
-                            echo "</br>";
-                            echo '<td><a href="?act=del&id='.$rs->id_Cargo.'">Confirmar Exclusão</a></td>';
-                        }
-                    } else {
-                    echo "<script> 
-                    alert('IMPOSSIVEL APAGAR CARGO POIS ESTÁ SENDO USADO EM OUTRA PÁGINA!'); 
-                    window.location.href='index.php';  
-                    </script>";
-                    }
-                } catch (PDOException $erro) {
-                    echo "Erro: " . $erro->getMessage();
-                }
-            ?>
-        </tbody>
-    </table>
+    <div class="box-p">
+        <div class="box-f1">
+            <h1>Excluir</h1>
+            <div class="box-f3">
+                <h2 class="title-02">Consultar</h4>
+                <div class="box-f4">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Descrição</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                try {
+                                    $stmt = $conexao->prepare("SELECT * FROM g4_cargo WHERE id_Cargo= :id");
+                                    $stmt->bindParam(":id", $id_Cargo, PDO::PARAM_INT); 
+                                    if ($stmt->execute()) {
+                                        while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
+                                            echo "<tr>";
+                                            echo "<td>$rs->id_Cargo</td>";
+                                            echo "<td>$rs->nome</td>";
+                                            echo "</tr>";
+                                            echo "</br>";
+                                            echo "<td>
+                                            <button><a href='?act=del&id=".$rs->id_Cargo."'>Confirmar Exclusão</a></button>
+                                            </td>";
+                                            echo "<td>
+                                            <button><a href='./index.php'>Voltar</a></button>
+                                            </td>";
+                                        }
+                                    } else {
+                                    echo "<script> 
+                                    alert('IMPOSSIVEL APAGAR CARGO POIS ESTÁ SENDO USADO EM OUTRA PÁGINA!'); 
+                                    window.location.href='index.php';  
+                                    </script>";
+                                    }
+                                } catch (PDOException $erro) {
+                                    echo "Erro: " . $erro->getMessage();
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
-    <?php
-        // ação de exclusão
-        //DEL
-            if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id_Cargo != ""){
-                try {
-                    $stmt = $conexao->prepare("DELETE FROM g4_cargo WHERE id_Cargo= :id");
-                    $stmt->bindParam(":id", $id_Cargo, PDO::PARAM_INT); 
-                    if($stmt->execute()) {
-                        echo "<script> 
-                            alert('Registro excluido com sucesso!'); 
-                            window.location.href='index.php';  
-                            </script>";
-                        echo "<p class = 'txt_cargo_exlcuido'> Registro excluido com sucesso!</p>";
-                    } else {
-                        echo "<p>Erro: Não foi possível executar a declaração sql</p>";
+            <?php
+                // ação de exclusão
+                //DEL
+                    if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id_Cargo != ""){
+                        try {
+                            $stmt = $conexao->prepare("DELETE FROM g4_cargo WHERE id_Cargo= :id");
+                            $stmt->bindParam(":id", $id_Cargo, PDO::PARAM_INT); 
+                            if($stmt->execute()) {
+                                echo "<script> 
+                                    alert('Registro excluido com sucesso!'); 
+                                    window.location.href='index.php';  
+                                    </script>";
+                                echo "<p class = 'txt_cargo_exlcuido'> Registro excluido com sucesso!</p>";
+                            } else {
+                                echo "<p>Erro: Não foi possível executar a declaração sql</p>";
+                            }
+                        } catch (PDOException $erro) {
+                            echo "<script> 
+                                alert('IMPOSSIVEL APAGAR CARGO POIS ESTÁ SENDO USADO EM OUTRA PÁGINA'); 
+                                window.location.href='index.php';  
+                                </script>";
+                            echo "IMPOSSIVEL APAGAR CARGO POIS ESTÁ SENDO USADO EM OUTRA PÁGINA ";
+                    
+                        }
                     }
-                } catch (PDOException $erro) {
-                    echo "<script> 
-                        alert('IMPOSSIVEL APAGAR CARGO POIS ESTÁ SENDO USADO EM OUTRA PÁGINA'); 
-                        window.location.href='index.php';  
-                        </script>";
-                    echo "IMPOSSIVEL APAGAR CARGO POIS ESTÁ SENDO USADO EM OUTRA PÁGINA ";
-            
-                }
-            }
-    ?>
-     <br>
-    <h2><a href="./index.php">Voltar</a></h2>
+            ?>
+        </div>
+    </div>
 </body>
 </html>
